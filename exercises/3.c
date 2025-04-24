83efd6eab2b92fef81ed34751a1429c2cdb9c8af
Artur Aronov <unionco@icloud.com>
2025-04-24 11:03:15 +0800

    exercises/3.c: complete

--- exercises/3.c
+++ exercises/3.c
@@ -29,18 +29,23 @@ char *to_path(char *req) {
     }
 
     // Ensure there's a '/' right before where we're about to copy in "index.html"
-    if (end[-1] == '/') {
-        end--; // We end in a slash, e.g. "/blog/" - so just move `end` to that slash.
-    } else {
+    if (end[-1] != '/') {
         end[0] = '/'; // We don't end in a slash, so write one.
+        end++;
     }
 
+    if (start[0] == '/') start++;
+
     // Copy in "index.html", overwriting whatever was there in the request string.
+    if(end + strlen(DEFAULT_FILE) + 1 > req + strlen(req)) return NULL;
+
     memcpy(
         // 👉 Try refactoring out this + 1 by modifying the `if/else` above.
-        end + 1,
+        // 💡 DONE
+        end,
         DEFAULT_FILE,
         // 👉 Try removing the +1 here. Re-run to see what happens, but first try to guess!
+        // 💡 DONE
         strlen(DEFAULT_FILE) + 1
     );
 
@@ -48,16 +53,6 @@ char *to_path(char *req) {
 }
 
 int main() {
-    // 👉 These three don't currently trim off the leading '/' - modify to_path to fix them!
-    char req1[] = "GET /blog HTTP/1.1\nHost: example.com";
-    printf("Should be \"blog/index.html\": \"%s\"\n", to_path(req1));
-
-    char req2[] = "GET /blog/ HTTP/1.1\nHost: example.com";
-    printf("Should be \"blog/index.html\": \"%s\"\n", to_path(req2));
-
-    char req3[] = "GET / HTTP/1.1\nHost: example.com";
-    printf("Should be \"index.html\": \"%s\"\n", to_path(req3));
-
     // 👉 Before fixing this next one, try moving it up to the beginning of main().
     //    What happens?
 
@@ -66,6 +61,19 @@ int main() {
     //    Hint: `strlen()` returns an integer whose type is not `int` but rather `size_t`
     char req4[] = "GET /blog ";
     printf("Should be \"(null)\": \"%s\"\n", to_path(req4));
+
+
+    // 👉 These three don't currently trim off the leading '/' - modify to_path to fix them!
+    // 💡 DONE
+
+    char req1[] = "GET /blog HTTP/1.1\nHost: example.com";
+    printf("Should be \"blog/index.html\": \"%s\"\n", to_path(req1));
+
+    char req2[] = "GET /blog/ HTTP/1.1\nHost: example.com";
+    printf("Should be \"blog/index.html\": \"%s\"\n", to_path(req2));
+
+    char req3[] = "GET / HTTP/1.1\nHost: example.com";
+    printf("Should be \"index.html\": \"%s\"\n", to_path(req3));
 
     return 0;
 }
